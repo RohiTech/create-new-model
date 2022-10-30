c4d8d38bcd4dbc83d773ca80eccec3c722576b35
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2022-10-30 17:03:38 -0600

    Create Eve Tables SQL

--- eve_tables.sql
+++ eve_tables.sql
@@ -16,6 +16,42 @@ create table eve_main
 	m_product_id numeric(10,0)
 );
 
-create unique index eve_main_uu_idx on eve_main
+create unique index eve_main_uu_idx
+on eve_main
 using btree
 (eve_main_uu collate pg_catalog."default");
+
+create unique index eve_main_value
+on eve_main
+using btree
+(ad_client_id, value collate pg_catalog."default");
+
+create table eve_sub
+(
+	eve_sub_id numeric(10,0) primary key not null,
+	eve_sub_uu character varying(36) default null::character varying,
+	ad_client_id numeric(10,0) not null,
+	ad_org_id numeric(10,0) not null,
+	isactive character(1) not null default 'Y'::bpchar,
+	created timestamp without time zone not null default now(),
+	createdby numeric(10,0) not null,
+	updated timestamp without time zone not null default now(),
+	updatedby numeric(10,0) not null,
+	eve_main_id numeric(10,0) not null,
+	value varchar(40) not null,
+	name varchar(255) not null,
+	description varchar(255)
+);
+
+create unique index eve_sub_uu_idx
+on eve_sub
+using btree
+(eve_sub_uu collate pg_catalog."default");
+
+create unique index eve_sub_value
+on eve_sub
+using btree
+(ad_client_id, value collate pg_catalog."default");
+
+
+
